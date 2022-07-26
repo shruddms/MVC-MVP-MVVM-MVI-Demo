bc047d73db398c39358b884c109cc588e9a33ca7
KyungEun No <74607521+shruddms@users.noreply.github.com>
2022-07-26 20:01:15 +0900

    Initial commit

---
+++ app/src/main/java/com/kyungeun/mvc_mvp_mvvm_mvi_demo/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.kyungeun.mvc_mvp_mvvm_mvi_demo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

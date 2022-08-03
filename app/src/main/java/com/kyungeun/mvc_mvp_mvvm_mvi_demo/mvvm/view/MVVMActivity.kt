64d1c899da00bb23eb64a3b29c5c837c02a2ba6c
KyungEun No <74607521+shruddms@users.noreply.github.com>
2022-08-03 13:18:07 +0900

    feat : Add mvi design pattern

--- app/src/main/java/com/kyungeun/mvc_mvp_mvvm_mvi_demo/mvvm/view/MVVMActivity.kt
+++ app/src/main/java/com/kyungeun/mvc_mvp_mvvm_mvi_demo/mvvm/view/MVVMActivity.kt
@@ -1,16 +1,12 @@
 package com.kyungeun.mvc_mvp_mvvm_mvi_demo.mvvm.view
 
-import android.R
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.kyungeun.mvc_mvp_mvvm_mvi_demo.databinding.ActivityMvvmBinding
-import com.kyungeun.mvc_mvp_mvvm_mvi_demo.mvp.view.MVPResultActivity
 import com.kyungeun.mvc_mvp_mvvm_mvi_demo.mvvm.viewmodel.MVVMViewModel
 import com.kyungeun.mvc_mvp_mvvm_mvi_demo.mvvm.viewmodel.eventObserve
 
@@ -20,7 +16,7 @@ class MVVMActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMvvmBinding
 
     private val viewModel: MVVMViewModel by lazy {
-        ViewModelProvider(this).get(MVVMViewModel::class.java)
+        ViewModelProvider(this)[MVVMViewModel::class.java]
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,9 +31,9 @@ class MVVMActivity : AppCompatActivity() {
     //변경된 데이터를 활용하여 View 업데이트 처리 동작
     @SuppressLint("SetTextI18n")
     private fun initObserve() {
-        viewModel.liveData.observe(this, Observer { memo ->
-            Log.d("liveData observe",memo.toString())
-        })
+        viewModel.liveData.observe(this) { memo ->
+            Log.d("liveData observe", memo.toString())
+        }
 
         viewModel.openEvent.eventObserve(this) { memo ->
             Log.d("openEvent eventObserve",memo.toString())
